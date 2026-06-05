48b4e66926d86b2bef14c4691369fbc0419bf74c
XavierAncient <xaviertovar450@gmail.com>
2026-06-04 20:10:43 -0400

    Se creó mensaje de bienvenida personalizado Integrante: Javier Tovar V-30555735

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string name;
+
+    cout<<"Ingrese su nombre para bienvenida personalizada: ";
+    cin>>name;
+    cout<<endl<<"Hola, "<<name<<" Bienvenido/a"<<endl;
 
     return 0;
 }
