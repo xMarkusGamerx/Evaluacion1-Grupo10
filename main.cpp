014ca2f1dacb53c83306d8747045bce83e96d7e4
wilkerpardoz2-lgtm <wilkerpardoz2@gmail.com>
2026-06-04 21:20:16 -0400

    Se agrego lista de integrantes al mensaje de bienvenida Integrante: Wilker Pardo V-28.247.132

--- main.cpp
+++ main.cpp
@@ -6,8 +6,16 @@ int main() {
     string name;
 
     cout<<"Ingrese su nombre para bienvenida personalizada: ";
-    cin>>name;
-    cout<<endl<<"Hola, "<<name<<" Bienvenido/a"<<endl;
+    getline(cin, name);
+    cout<<endl<<"Hola, "<<name<<" Bienvenido/a al sistema del grupo #10"<<endl;
+    cout << "\n========================================" << endl;
+    cout << "              INTEGRANTES" << endl;
+    cout << "========================================" << endl;
+    cout << "* Manuel Ruiz     - C.I: V-30.595.959" << endl;
+    cout << "* Javier Tovar    - C.I: V-30.555.735" << endl;
+    cout << "* Wilker Pardo    - C.I: V-28.247.132" << endl;
+    cout << "* Yuliani Mota    - C.I: V-30.645.387" << endl;
+    cout << "========================================" << endl;
 
     return 0;
 }
